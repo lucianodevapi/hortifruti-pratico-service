e2183e191d6e7d1af32e2829d83f02baa6895e3a
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2022-07-06 15:52:39 -0300

    Configurações iniciais

--- src/main/java/com/hortifruti/api/Application.java
+++ src/main/java/com/hortifruti/api/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HortifrutiPraticoServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HortifrutiPraticoServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
