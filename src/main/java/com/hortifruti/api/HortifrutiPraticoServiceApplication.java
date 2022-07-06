8107d5f9bd83115cf88665d3e5632ebb9636e33d
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2022-07-06 15:40:34 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/hortifruti/api/HortifrutiPraticoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hortifruti.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortifrutiPraticoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortifrutiPraticoServiceApplication.class, args);
+	}
+
+}
